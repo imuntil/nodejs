300896671053d838b951eeb3e8f9f6b669c94b5c
zhin <imuntil@qq.com>
2021-07-02 08:23:06 +0800

    feat: 心跳监测； feat: 进程守护

--- re/node.xx/cluster/app.ts
+++ re/node.xx/cluster/app.ts
@@ -4,10 +4,14 @@ import http from 'http'
 const server = http
   .createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/html' })
-    res.end(fs.readFileSync(__dirname + '/index.htm', 'utf-8'))
+    setTimeout(() => {
+      // 做一些错误的操作
+      ;(window as any).__xx = 'xxx'
+      res.end(fs.readFileSync(__dirname + '/index.htm', 'utf-8'))
+    }, 100)
   })
   .listen(3000, () => {
-    console.log('server runing at 3000')
+    console.log('server runing  at 3000')
   })
 
 export default server
--- re/node.xx/cluster/index.ts
+++ re/node.xx/cluster/index.ts
@@ -1,11 +1,73 @@
 import cluster from 'cluster'
 import os from 'os'
 
+const createWorker = () => {
+  const worker = cluster.fork()
+  let itv: any
+  let missed = 0
+  // 心跳监测：定时发送ping
+  itv = setInterval(() => {
+    worker.send('ping')
+    console.log(`missed`, missed)
+    // 连续3次无响应，退出进程
+    if (missed >= 3) {
+      process.kill(worker.process.pid)
+    }
+    missed++
+  }, 3000)
+
+  // 心跳监测：收到pong，重置记录
+  worker.on('message', (msg: string) => {
+    console.log(msg)
+    msg === 'pong' && (missed = 0)
+  })
+
+  // 监测到当前进程退出，删除心跳监测
+  worker.on('exit', () => {
+    clearInterval(itv)
+  })
+}
+
 if (cluster.isMaster) {
-  const cpus = os.cpus().length >> 1
+  const cpus = os.cpus().length >> 2 || 1
   for (let i = 0; i < cpus; i++) {
-    cluster.fork()
+    createWorker()
   }
+
+  // 监听到进程退出，重新开启一个进程
+  cluster.on('exit', () => {
+    setTimeout(() => {
+      // 为了防止进入死循环，做个延时处理
+      createWorker()
+    }, 5e3)
+  })
 } else {
   import('./app')
+
+  // 监听未捕获错误
+  process.on('uncaughtException', (err) => {
+    console.error('err:', err)
+    // 退出进程
+    process.exit(1)
+  })
+
+  // 内存检测
+  setInterval(() => {
+    // 进程占用内存超过某个值，退出该进程
+    if (process.memoryUsage().rss > 1024 * 1024 * 700) {
+      console.log('xxxxxxx over memory')
+      process.exit(1)
+    }
+  }, 5e3)
+
+  // 心跳监测：收到主进程的ping，回复pong
+  process.on('message', (msg: string) => {
+    console.log(msg)
+    msg === 'ping' && process.send('pong')
+  })
+
+  // 心跳监测：模拟进程假死（还在执行，但是已经不能响应任何消息）
+  setTimeout(() => {
+    while (true) {}
+  }, 5e3)
 }
